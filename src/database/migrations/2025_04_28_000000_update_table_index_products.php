79c0e653660353722d0dc8633203b3178bba01b5
Eduard <63282723+Eduardito187@users.noreply.github.com>
2025-04-28 14:49:05 -0400

    Update index products

---
+++ src/database/migrations/2025_04_28_000000_update_table_index_products.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('index_products', function (Blueprint $table) {
+            $table->integer('index_priority')->default(0)->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('index_products', function (Blueprint $table) {
+            $table->dropColumn('index_priority');
+        });
+    }
+};
