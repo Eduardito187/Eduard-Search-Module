418b8bb9aff4e069e9259172372ae91e518e60cd
Eduard <63282723+Eduardito187@users.noreply.github.com>
2025-05-23 15:37:40 -0400

    Update DisabledIndexProducts.php

--- src/Console/Commands/DisabledIndexProducts.php
+++ src/Console/Commands/DisabledIndexProducts.php
@@ -32,6 +32,7 @@ class DisabledIndexProducts extends Command
     {
         $threshold = Carbon::now()->subHours(72);
         DB::table('index_products as ip')->join('product as p', 'ip.id_product', '=', 'p.id')->where('p.updated_at', '<', $threshold)->update(['ip.status' => 0]);
+        DB::table('product')->where('updated_at', '<', $threshold)->update(['status' => 0]);
         Log::channel('disabledIndexProducts')->info("Cron disabledIndexProducts ejecutado.");
         return Command::SUCCESS;
     }
