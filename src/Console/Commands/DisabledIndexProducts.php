b2e759c1c1efa8404e1a58fac8d69b3db785acbb
Eduard <63282723+Eduardito187@users.noreply.github.com>
2025-05-23 10:08:29 -0400

    Update DisabledIndexProducts.php

--- src/Console/Commands/DisabledIndexProducts.php
+++ src/Console/Commands/DisabledIndexProducts.php
@@ -2,14 +2,10 @@
 
 namespace Eduard\Search\Console\Commands;
 
-use Eduard\Search\Models\Attributes;
-use Eduard\Account\Models\Client;
-use Eduard\Search\Models\IndexProducts;
-use Eduard\Search\Models\Product;
-use Eduard\Search\Models\ProductAttribute;
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class DisabledIndexProducts extends Command
 {
@@ -34,39 +30,8 @@ class DisabledIndexProducts extends Command
      */
     public function handle()
     {
-        $fechaLimite = Carbon::now()->subHours(24);
-        IndexProducts::where('status', true)->where('updated_at', '<', $fechaLimite)->update(['status' => false]);
-        $allClient = Client::all();
-
-        foreach ($allClient as $key => $client) {
-            $price = Attributes::where('code', 'price')->where('id_client', $client->id)->first();
-
-            if (!$price) {
-                foreach ($client->indexes as $key => $index) {
-                    Product::where('status', true)->where('id_client', $client->id)->update(['status' => false]);
-                }
-            } else {
-                foreach ($client->indexes as $key => $index) {
-                    $filteredProducts = ProductAttribute::where('id_attribute', $price->id)
-                    ->where(function ($query) {
-                        $query->where('value', null)
-                            ->orWhere('value', '<', 1);
-                    })->pluck('id_product')->toArray();
-                    Product::where('status', true)->whereIn('id', $filteredProducts)->update(['status' => false]);
-
-                    $productsWithoutAttributes = Product::whereNotIn('id', function ($query) {
-                        $query->select('id_product')->from('product_attribute')->where('id_attribute', 1)
-                            ->where(function ($subquery) {
-                                $subquery->where('value', null)
-                                    ->orWhere('value', '<', 1);
-                            });
-                    })->pluck('id')->toArray();
-                    Product::where('status', true)->whereIn('id', $productsWithoutAttributes)->update(['status' => false]);
-                }
-            }
-        }
-
-
+        $threshold = Carbon::now()->subHours(72);
+        DB::table('index_products as ip')->join('product as p', 'ip.id_product', '=', 'p.id')->where('p.updated_at', '<', $threshold)->update(['ip.state' => 0]);
         Log::channel('disabledIndexProducts')->info("Cron disabledIndexProducts ejecutado.");
         return Command::SUCCESS;
     }
