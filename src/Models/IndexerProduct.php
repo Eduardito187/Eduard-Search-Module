e7245913344ff44c2c7fc7ef74863afdaea92c0c
Eduard <63282723+Eduardito187@users.noreply.github.com>
2025-05-27 23:59:48 -0400

    Update IndexerProduct.php

--- src/Models/IndexerProduct.php
+++ src/Models/IndexerProduct.php
@@ -2,10 +2,11 @@
 
 namespace Eduard\Search\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Eduard\Search\Models\IndexCatalog;
 use Eduard\Search\Models\Product;
+use Eduard\Search\Models\IndexCatalog;
+use Eduard\Search\Models\IndexProducts;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class IndexerProduct extends Model
 {
@@ -22,14 +23,24 @@ class IndexerProduct extends Model
     /**
      * @inheritDoc
      */
-    public function getProduct() {
+    public function getProduct()
+    {
         return $this->hasOne(Product::class, 'id', 'id_product');
     }
 
     /**
      * @inheritDoc
      */
-    public function getIndex() {
+    public function getIndex()
+    {
         return $this->hasOne(IndexCatalog::class, 'id', 'id_index_catalog');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function indexes()
+    {
+        return IndexProducts::query()->whereColumn('id_product', 'index_products.id_product')->whereColumn('id_index_catalog', 'index_products.id_index_catalog');
     }
 }
