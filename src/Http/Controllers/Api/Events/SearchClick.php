29676e2c418d8688eb0125116b5243a72b1f6a8d
Eduard <63282723+Eduardito187@users.noreply.github.com>
2025-10-21 14:09:12 -0400

    Create SearchClick.php

---
+++ src/Http/Controllers/Api/Events/SearchClick.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Eduard\Search\Http\Controllers\Api\Events;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+class SearchClick extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'uuid'=> ['required','string','size:36'],
+            'product_id' => ['required','integer','min:1'],
+            'rank'=> ['nullable','integer','min:1'],
+        ]);
+
+        DB::insert(
+            "INSERT INTO search_clicks (request_uuid, product_id, rank, clicked_at, referer, client_ip, ua)
+             VALUES (?, ?, ?, NOW(), ?, INET6_ATON(?), ?)",
+            [
+                $data['uuid'],
+                $data['product_id'],
+                $data['rank'] ?? null,
+                substr((string) $request->headers->get('referer'), 0, 255),
+                $request->ip(),
+                substr((string) $request->userAgent(), 0, 255),
+            ]
+        );
+
+        return response()->noContent();
+    }
+}
