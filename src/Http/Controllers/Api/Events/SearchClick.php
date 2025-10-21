070a5bdb13ed1268a05b88ba5f383ad9d7edf8c8
Eduard <63282723+Eduardito187@users.noreply.github.com>
2025-10-21 14:57:46 -0400

    Update SearchClick.php

--- src/Http/Controllers/Api/Events/SearchClick.php
+++ src/Http/Controllers/Api/Events/SearchClick.php
@@ -12,17 +12,19 @@ class SearchClick extends Controller
     {
         $data = $request->validate([
             'uuid'=> ['required','string','size:36'],
+            'query'=> ['required','string'],
             'product_id' => ['required','integer','min:1'],
             'rank'=> ['nullable','integer','min:1'],
         ]);
 
         DB::insert(
-            "INSERT INTO search_clicks (request_uuid, product_id, rank, clicked_at, referer, client_ip, ua)
-             VALUES (?, ?, ?, NOW(), ?, INET6_ATON(?), ?)",
+            "INSERT INTO search_clicks (request_uuid, product_id, rank, clicked_at, raw_query_norm, referer, client_ip, ua)
+             VALUES (?, ?, ?, NOW(), ?, ?, INET6_ATON(?), ?)",
             [
                 $data['uuid'],
                 $data['product_id'],
                 $data['rank'] ?? null,
+                $data['query'] ?? '',
                 substr((string) $request->headers->get('referer'), 0, 255),
                 $request->ip(),
                 substr((string) $request->userAgent(), 0, 255),
